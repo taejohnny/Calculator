5faa8a492d2129dae9ceec516c05f637bda152aa
Taesan Yoon <taesanyoon@umass.edu>
2017-08-27 21:28:59 -0400

    first commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,3 @@
-/*This program is designed to be a simple way for Umass Amherst students to calculate their GPA in
- * a given semester. Although calculating GPA doesn't take very long, it can become tedious when you
- * are someone like me, who is constantly checking their grades. The way Umass calculates its GPA is by
- * first multiplying the number of credits in a class by the grade point value received in that class. This
- * is done for all of the classes, and these resulting numbers are added up. This total number is then 
- * divided by the total number of credits attempted in the semester. Again, this is a short process that 
- * takes usually only 5 minutes, but with this calculator, the process can be just a couple of seconds. I
- * initially made this program as a side project and a convenient way to calculate my grade in the future,
- * but my friends have also taken a liking to this program and have asked to use this program many times.*/
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
